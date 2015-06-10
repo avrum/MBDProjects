f9b320797961b28696a7a75742c839869d6af42f
nivave <nivaverbuch@gmail.com>
2015-06-10 18:38:29 +0300

    cleaning Program.cs (fix mistakly previous commit )

--- DiagnosisProjects/Program.cs
+++ DiagnosisProjects/Program.cs
@@ -13,36 +13,9 @@ namespace DiagnosisProjects
     {
         static void Main(string[] args)
         {
-            HashSet<int> numberSet1 = new HashSet<int>();
-            numberSet1.Add(1);
-            numberSet1.Add(2);
-            HashSet<int> numberSet2 = new HashSet<int>();
-            numberSet2.Add(2);
-            numberSet2.Add(3);
-            numberSet2.Add(4);
-            HashSet<int> numberSet3 = new HashSet<int>();
-            numberSet3.Add(4);
-            numberSet3.Add(5);
-
-            List<HashSet<int> > setsList = new List<HashSet<int>>();
-            setsList.Add(numberSet1);
-            setsList.Add(numberSet2);
-            setsList.Add(numberSet3);
-            List<HashSet<int>> hittingSets = SwitchingAlgorithmHittingSetFinder.findHittingSet(setsList, 5);
-            printSetList(hittingSets);
+   
         }
 
-        private static void printSetList(List<HashSet<int>> hittingSets)
-        {
-            foreach (HashSet<int> hittingSet in hittingSets)
-            {
-                Console.Write("{ ");
-                foreach (int i in hittingSet)
-                {
-                    Console.Write(i+" ");
-                }
-                Console.WriteLine("}");
-            }
-        }
+  
     }
 }
