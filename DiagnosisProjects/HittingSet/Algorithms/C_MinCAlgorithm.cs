c1f3aa6c016155596bfdeee51cea9e1c9cbcf4d6
asafshen <asafshen86@gmail.com>
2015-06-30 19:14:25 +0300

    MinC Algorithms

---
+++ DiagnosisProjects/HittingSet/Algorithms/C_MinCAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosisProjects.HittingSet.Algorithms
+{
+    static class C_MinCAlgorithm 
+    {
+        static int minc = int.MaxValue;
+        public static int FindMinC(ConflictSet conflicts)
+        {
+            FindMinCHelper(conflicts, 0);
+            return minc;
+        }
+
+        private static void FindMinCHelper(ConflictSet conflicts, int m)
+        {
+            if (m >= minc || MinC_Utils.containEmptySet(conflicts) == true)
+            { //1st & 3rd base case
+                return;
+            }
+            else if (MinC_Utils.isConflictSetEmpty(conflicts) == true)
+            {  //2nd base case
+                minc = m;
+                return;
+            }
+            // Dual Reduce
+
+            // compute conflicts
+            Gate s = MinC_Utils.getMostfrequentlyComp(conflicts);
+            ConflictSet conflictsMinusS = MinC_Utils.ConflictsMinusComponent(conflicts, s);
+            ConflictSet conflictsWithoutS = MinC_Utils.ConflictsWithoutComponent(conflicts, s);
+
+            FindMinCHelper(conflictsMinusS, m);
+            FindMinCHelper(conflictsWithoutS, m + 1);
+        }
+    }
+}
---
+++ DiagnosisProjects/HittingSet/Unittests/R_MinCUnittest_2.cs
@@ -0,0 +1,35 @@
+﻿using DiagnosisProjects.HittingSet.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosisProjects.HittingSet.Unittests
+{
+    class R_MinCUnittest_2
+    {
+        public static void RunTest()
+        {
+            ModelObservationCreator cc = new ModelObservationCreator();
+            List<Observation> a = cc.ReadObsModelFiles("111.txt", "111_obs.txt");
+            Observation bObservation = a[0];
+
+            List<Gate> allComponents = bObservation.TheModel.Components;
+
+            // 11 , 22 , 33
+            // 44 , 22 , 55
+
+            Conflict c1 = new Conflict(new List<Gate>() { allComponents[0], allComponents[2], allComponents[3] });
+            Conflict c2 = new Conflict(new List<Gate>() { allComponents[1], allComponents[2], allComponents[4] });
+
+            ConflictSet cs = new ConflictSet();
+            cs.Conflicts = new List<Conflict>() { c1, c2 };
+
+            int minc = R_MinCAlgorithm.FindMinC(cs);
+            Console.WriteLine("R_MinC Unit test 2. minc = " + minc);
+
+            int x = 0;
+        }
+    }
+}
