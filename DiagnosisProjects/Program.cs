b240e5f40295ce6e898d6ccba81b6c4627edc39d
michaldan100 <michaldan100@gmail.com>
2015-07-07 20:19:10 +0300

    ltms

--- DiagnosisProjects/Program.cs
+++ DiagnosisProjects/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DiagnosisProjects.HittingSet;
 using Microsoft.SolverFoundation.Solvers;
+using DiagnosisProjects.LTMS;
 
 namespace DiagnosisProjects
 {
@@ -12,6 +13,19 @@ namespace DiagnosisProjects
     {
         static void Main(string[] args)
         {
+            ModelObservationCreator cc = new ModelObservationCreator();
+            List<Observation> a = cc.ReadObsModelFiles("777.txt", "777_iscas85.txt");
+            //List<Observation> a = cc.ReadObsModelFiles("74181.txt", "74181_iscas85.txt");
+            SystemModel ss = a[0].TheModel;
+            foreach (Observation o in a)
+            {
+                LtmsAlgorithm ltms = new LtmsAlgorithm(ss, o);
+                List<List<Gate>> conf = ltms.findConflicts();
+            }
+
         }
+
+
+
     }
 }
