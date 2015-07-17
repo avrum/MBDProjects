8c41504e1bda600055b13b4d978a1abfb307d116
asafshen <asafshen86@gmail.com>
2015-07-17 13:49:43 +0300

    Minc Algorithms

---
+++ DiagnosisProjects/HittingSet/Algorithms/A_MinCAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosisProjects.HittingSet.Algorithms
+{
+    class A_MinCAlgorithm
+    {
+        static int infinity = int.MaxValue;
+
+        public static MicC_Diagnosis FindMinC(ConflictSet conflicts)
+        {
+            int minc = infinity;
+
+            if (MinC_Utils.isConflictSetEmpty(conflicts) == true)
+            {  //1st base case
+                return new MicC_Diagnosis();
+            }
+            else if (MinC_Utils.containEmptySet(conflicts) == true)
+            { //2nd base case
+                MicC_Diagnosis infinityDiagnosis = new MicC_Diagnosis();
+                infinityDiagnosis.cardinality = infinity;
+                return infinityDiagnosis;
+            }
+
+            Gate s = MinC_Utils.getMostfrequentlyComp(conflicts);
+            ConflictSet conflictsWithoutS = MinC_Utils.ConflictsWithoutComponent(conflicts, s);
+
+            MicC_Diagnosis mincDiagnosisContaintS = FindMinC(conflictsWithoutS);
+            
+            mincDiagnosisContaintS.AddCompToDiagnosis(s);
+            return mincDiagnosisContaintS;
+
+        }
+    }
+}
---
+++ DiagnosisProjects/HittingSet/Unittests/BB_MinC/BB_MinCUnittest_2cs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosisProjects.HittingSet.Unittests.BB_MinC
+{
+    class BB_MinCUnittest_2cs
+    {
+    }
+}
